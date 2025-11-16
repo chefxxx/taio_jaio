a812c539ab31f63f1dbd11181e8d730cabc908f5
chefxxx <mateusz.mm100@gmail.com>
2025-11-16 18:51:23 +0100

    Add test file

---
+++ tests/parser_test.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Mateusz Mikiciuk on 16/11/2025.
+//
+
+#include <gtest/gtest.h>
+#include "io_manager.h"
+
+TEST(ParserTest, smallMatricesReadoutSuite)
+{
+    const std::string fileName = "../../sample_files/small_graphs_test.txt";
+    Eigen::Matrix<int, 6, 6> first = {
+        {}
+    };
+}
