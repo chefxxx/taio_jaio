5e9509d5f54c0664df01ba63c569f5e3b0c914fc
chefxxx <mateusz.mm100@gmail.com>
2025-11-16 18:12:33 +0100

    init branch

--- project/src/io_manager.cpp
+++ project/src/io_manager.cpp
@@ -4,11 +4,12 @@
 
 #include <fstream>
 #include <spdlog/spdlog.h>
+#include <Eigen/Dense>
 #include "io_manager.h"
 
 void parseInputFile(const std::string_view t_inputFileName)
 {
-    const std::ifstream inputFile(t_inputFileName);
+    const std::ifstream inputFile(t_inputFileName.data());
     if (!inputFile.is_open()) {
         spdlog::error("Failed to open file {}", t_inputFileName);
         exit(EXIT_FAILURE);
