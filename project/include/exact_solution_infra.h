e101d4fe7b528f39ab7f8f453f23111b3d38e3a2
chefxxx <mateusz.mm100@gmail.com>
2025-11-18 14:21:47 +0100

    Add findOrExtend skelly

--- project/include/exact_solution_infra.h
+++ project/include/exact_solution_infra.h
@@ -5,12 +5,13 @@
 #ifndef EXACT_SOLUTION_INFRA_H
 #define EXACT_SOLUTION_INFRA_H
 
+#include <optional>
+
 #include "eigen_port.h"
 
 struct SI_Problem
 {
   SI_Problem(const Matrix& t_A1, const Matrix& t_A2) : A1(t_A1), A2(t_A2), v1(t_A1.rows()), v2(t_A2.rows()) {}
-
   const Matrix& A1;
   const Matrix& A2;
   long v1;
@@ -20,14 +21,15 @@ struct SI_Problem
 struct SI_State
 {
   SI_State(const std::vector<bool>& t_cols, const Matrix& t_M, const long t_R) : cols(t_cols), M(t_M), R(t_R) {}
-  void serialNextState(const int col) { M(R,col) = 1; R++; cols[col] = true; }
-  void serialPrevState(const int col) { cols[col] = false; R--; M(R, col) = 0; }
+  void serialNextState(const long col) { M(R,col) = 1; R++; cols[col] = true; }
+  void serialPrevState(const long col) { cols[col] = false; R--; M(R, col) = 0; }
   std::vector<bool> cols;
   Matrix M;
   long R;
 };
 
 void performExactAlgorithm(const Matrix &t_A1, const Matrix &t_A2, int t_k);
-void subgraphIsomorphismSerial(const SI_Problem &P, SI_State &state);
+void subgraphIsomorphismSerial(const SI_Problem &t_P, SI_State &t_state);
+void findOrExtend(const Matrix& t_A1, const Matrix& t_A2, const Matrix& t_M);
 
 #endif // EXACT_SOLUTION_INFRA_H
--- project/src/exact_solution_infra.cpp
+++ project/src/exact_solution_infra.cpp
@@ -3,9 +3,11 @@
 //
 
 #include "exact_solution_infra.h"
-#include "common.h"
 
+#include <iostream>
 #include <spdlog/spdlog.h>
+
+#include "common.h"
 
 void performExactAlgorithm(const Matrix &t_A1, const Matrix &t_A2, const int t_k)
 {
@@ -35,25 +37,43 @@ void performExactAlgorithm(const Matrix &t_A1, const Matrix &t_A2, const int t_k
     // ------------------
     const SI_Problem globalState{t_A1, A2};
     SI_State initState{cols, M, 0};
+    spdlog::info("Procedure SubgraphIsomorphismSerial run...");
     subgraphIsomorphismSerial(globalState, initState);
 }
 
-void subgraphIsomorphismSerial(const SI_Problem &P, SI_State &state)
+void subgraphIsomorphismSerial(const SI_Problem &t_P, SI_State &t_state)
 {
-    spdlog::info("Procedure SubgraphIsomorphismParallel run...");
-    if (state.R == P.v1) {
+    if (t_state.R == t_P.v1) {
         // procedure find or extend run
+        std::cout << t_state.M << '\n';
         return;
     }
 
-    for (int i = 0; i < state.cols.size(); ++i) {
-        if (!state.cols[i]) {
+    for (size_t i = 0; i < t_state.cols.size(); ++i) {
+        if (!t_state.cols[i]) {
             // init new state
-            state.serialNextState(i);
+            t_state.serialNextState(i);
             // recurse
-            subgraphIsomorphismSerial(P, state);
+            subgraphIsomorphismSerial(t_P, t_state);
             // back to state
-            state.serialPrevState(i);
+            t_state.serialPrevState(i);
+        }
+    }
+}
+
+void findOrExtend(const Matrix &t_A1, const Matrix &t_A2, const Matrix &t_M)
+{
+    bool isValid = true;
+    const auto A1prim = t_M * t_A2 * t_M.transpose();
+    for (long i = 0; i < t_A1.rows(); ++i) {
+        for (long j = 0; j < t_A1.cols(); ++j) {
+            if (t_A1(i, j) > A1prim(i,j)) {
+                isValid = false;
+                break;
+            }
+        }
+        if (!isValid) {
+            break;
         }
     }
 }
