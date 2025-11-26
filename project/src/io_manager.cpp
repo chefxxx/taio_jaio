57106acbcd335fedba3e8c6ae47137c8ef34bd7d
chefxxx <mateusz.mm100@gmail.com>
2025-11-26 12:31:21 +0100

    Refactor dualPrint

--- project/src/io_manager.cpp
+++ project/src/io_manager.cpp
@@ -58,49 +58,50 @@ Matrix convertToMatrix(std::ifstream &inputFile, int& lineNumber)
     }
 }
 
+const std::string COLOR_YELLOW = "\033[93m";
+const std::string COLOR_RESET = "\033[0m";
+constexpr int WIDTH = 3;
+
+void dualPrint(const std::string &text, std::ofstream *outputFile, const int width = 0, const bool colored = false)
+{
+    if (colored)
+        std::cout << COLOR_YELLOW << std::setw(width) << text << COLOR_RESET;
+    else
+        std::cout << std::setw(width) << text;
+    if (outputFile != nullptr)
+        *outputFile << std::setw(width) << text;
+}
+
 void printMatricesAfterAlgorithm(const Matrix &originalMatrix, const Matrix &updatedMatrix, std::ofstream* outputFile)
 {
-    static const std::string COLOR_YELLOW = "\033[93m";
-    static const std::string COLOR_RESET = "\033[0m";
-    static const int WIDTH = 3;
     const int oldN = originalMatrix.rows();
     const int oldM = originalMatrix.cols();
     const int newN = updatedMatrix.rows();
     const int newM = updatedMatrix.cols();
 
-    auto dualPrint = [&](std::string text, int width = 0, bool colored = false) {
-        if (colored)
-            std::cout << COLOR_YELLOW << std::setw(width) << text << COLOR_RESET;
-        else
-            std::cout << std::setw(width) << text;
-        if (outputFile != nullptr)
-            *outputFile << std::setw(width) << text;
-    };
+    dualPrint("\n\n=== ORIGINAL MATRIX FOR GRAPH G2 ===\n\n", outputFile);
 
-    dualPrint("\n\n=== ORIGINAL MATRIX FOR GRAPH G2 ===\n\n");
-
-
-    for (int i = 0; i < oldN; ++i, dualPrint("\n")) {
+    for (int i = 0; i < oldN; ++i, dualPrint("\n", outputFile)) {
         for (int j = 0; j < oldM; ++j) {
-            int cost = originalMatrix(i, j);
-            dualPrint(std::to_string(cost), WIDTH);
+            const int cost = originalMatrix(i, j);
+            dualPrint(std::to_string(cost), outputFile, WIDTH);
         }
     }
 
-    dualPrint("\n\n=== EXTENDED MATRIX FOR GRAPH G2 ===\n\n");
+    dualPrint("\n\n=== EXTENDED MATRIX FOR GRAPH G2 ===\n\n", outputFile);
 
-    for (int i = 0; i < newN; ++i, dualPrint("\n")) {
+    for (int i = 0; i < newN; ++i, dualPrint("\n", outputFile)) {
         for (int j = 0; j < newM; ++j) {
             int cost = updatedMatrix(i, j);
             if (cost > originalMatrix(i, j)) {
-                dualPrint(std::to_string(cost), WIDTH, true);
+                dualPrint(std::to_string(cost), outputFile, WIDTH, true);
             } else {
-                dualPrint(std::to_string(cost), WIDTH);
+                dualPrint(std::to_string(cost), outputFile, WIDTH);
             }
         }
     }
 
-    dualPrint("\n\n");
+    dualPrint("\n\n", outputFile);
 }
 
 void printMultipleMappings(const std::vector<Matrix> &t_mappings, const int t_number)
