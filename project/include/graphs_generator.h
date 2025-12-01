e15b9c44378c0c78b4f3779d4b2888144ce910ad
MykhailoShamrai <mike.shamrai@gmail.com>
2025-12-01 18:49:31 +0100

    init

---
+++ project/include/graphs_generator.h
@@ -0,0 +1,12 @@
+//
+// Created by mshamrai on 12/1/25.
+//
+
+#ifndef TAIO_PROJECT_GRAPHS_GENERATOR_H
+#define TAIO_PROJECT_GRAPHS_GENERATOR_H
+
+#include "eigen_port.h"
+
+std::pair<Matrix> generateTwoGrapghs(int minN, int maxN, int maxMult, int seed);
+
+#endif // TAIO_PROJECT_GRAPHS_GENERATOR_H
